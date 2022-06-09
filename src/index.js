ff8a3a0499a267dab5e2257171ebe8051d4da867
Ted O'Connor <ted@opub.com>
2022-06-09 10:10:46 -0400

    added error handling in analyze

--- src/index.js
+++ src/index.js
@@ -9,30 +9,32 @@ function analyze(collections, balance) {
     const count = collections.length;
 
     for (let i = 0; i < collections.length; i++) {
-        let listings = collections[i].listings;
-        let fp = listings[0].price;
-        let cost = fp;
-        let end = Math.min(f.maxFloorSize, listings.length - 1);
-        let j = 1;
-        while (j < end) {
-            let floor = (listings[j].price - fp) / fp * 100.0;
-            if (floor <= f.floorThreshold) {
-                cost += listings[j].price;
-                j++;
-            } else {
-                break;
+        try {
+            let listings = collections[i].listings;
+            let fp = listings[0].price;
+            let cost = fp;
+            let end = Math.min(f.maxFloorSize, listings.length - 1);
+            let j = 1;
+            while (j < end) {
+                let floor = (listings[j].price - fp) / fp * 100.0;
+                if (floor <= f.floorThreshold) {
+                    cost += listings[j].price;
+                    j++;
+                } else {
+                    break;
+                }
+            }
+            if (cost < balance && j <= listings.length - 1) {
+                let wall = (listings[j].price - fp) / fp * 100.0
+                collections[i].wall = wall;
+                collections[i].profit = listings[j].price - fp - f.newFloorOffset;
             }
         }
-        if (cost < balance) {
-            let wall = (listings[j].price - fp) / fp * 100.0
-            collections[i].wall = wall;
-            collections[i].profit = listings[j].price - fp - f.newFloorOffset;
-        } else {
-            collections[i].wall = -1;
-            collections[i].profit = -1;
+        catch(e) {
+            log('ERROR analyze failed', collections[i].symbol, e);
         }
     }
-    collections = collections.filter(c => c.wall >= f.minWallSize && c.profit >= f.minProfit);
+    collections = collections.filter(c => c.wall && c.wall >= f.minWallSize && c.profit && c.profit >= f.minProfit);
     collections.sort(function (a, b) { return b.profit - a.profit });
     log('filtered', collections.length, 'collections of', count);
     return collections;
